8eabf6e98b436e98df459ca5a5a960539a373aad
constayush <aayush@mail.com>
2024-08-29 22:22:22 +0530

    basic setup

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 function AuthPage() {
   return (
-    <div>AuthPage</div>
+
+
+//write your code here, baki mat chediyo.
+
+
+
+
+
+
+    <div className='p-8 w-full h-[100dvh] bg-[#FAF3E0]'>
+
+
+      <h1 className='text-[6rem] logoFont text-[#1E212D]'>Recover'IT</h1>
+      <p className='p-2 text-[1.5rem] w-[60%] text-[#1E212D]'>Lose something on campus? Our Lost and Found app makes it easy to get your stuff back. Just report what you’ve lost, or if you find something, list it here. We’re all about helping you reconnect with your belongings quickly and hassle-free, so nothing important stays lost for long.</p>
+
+
+      <div className='flex gap-[2rem]'>
+        <button className='button-86 loginBtn '>Login</button>
+        <button className='button-86 signBtn '>Sign up</button>
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
   )
 }
 
