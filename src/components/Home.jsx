0a0215d8509559c9119580fe30ff8f7768c6e37f
constayush <aayush@mail.com>
2024-09-10 23:06:03 +0530

    searchbar ui added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,17 +5,31 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 import Navbar from './ui/Navbar'
 export function Home() {
-  return (<>
+  return (<div className="overflow-x-hidden">
 
 
   <Navbar/>
+
+
+<div className="mb-16">
+  <form class="max-w-md mx-auto">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-gray-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    </div>
+</form>
+</div>
+
   
   <div className="flex flex-wrap justify-center items-center gap-3 ">
-    
-    
-      
-    
-    <CardContainer className="item-card inter-var w-[20rem]  h-[20rem]">
+
+    <CardContainer className="item-card inter-var w-[20rem]  h-[20rem] mb-3">
       <CardBody
         className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
@@ -49,13 +63,13 @@ export function Home() {
           <CardItem
             translateZ={20}
           
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
+            className="claimBtn px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
             Claim
           </CardItem>
         </div>
       </CardBody>
     </CardContainer>
-    <CardContainer className="item-card inter-var w-[20rem]  h-[20rem]">
+    <CardContainer className="item-card inter-var w-[20rem]  h-[20rem] mb-3">
       <CardBody
         className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
@@ -89,13 +103,13 @@ export function Home() {
           <CardItem
             translateZ={20}
           
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
+            className="claimBtn px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
             Claim
           </CardItem>
         </div>
       </CardBody>
     </CardContainer>
-    <CardContainer className="item-card inter-var w-[20rem]  h-[20rem]">
+    <CardContainer className="item-card inter-var w-[20rem]  h-[20rem] mb-3">
       <CardBody
         className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
@@ -129,13 +143,13 @@ export function Home() {
           <CardItem
             translateZ={20}
           
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
+            className="claimBtn px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
             Claim
           </CardItem>
         </div>
       </CardBody>
     </CardContainer>
-    <CardContainer className="item-card inter-var w-[20rem]  h-[20rem]">
+    <CardContainer className="item-card inter-var w-[20rem]  h-[20rem] mb-3">
       <CardBody
         className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
@@ -169,16 +183,20 @@ export function Home() {
           <CardItem
             translateZ={20}
           
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
+            className="claimBtn px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
             Claim
           </CardItem>
         </div>
       </CardBody>
     </CardContainer>
 
+</div>  
 
-  
-</div>  </>);
+
+
+
+
+</div>);
 }
 
 
